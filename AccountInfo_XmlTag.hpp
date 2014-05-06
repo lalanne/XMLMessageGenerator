f777747f38bc1e41c76f2eeb6e4b05df9d4ff5cf
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-06 17:18:24 +0100

    adding AccountInfo_XmlTag class

---
+++ AccountInfo_XmlTag.hpp
@@ -0,0 +1,14 @@
+#ifndef ACCOUNT_INFO_XML_TAG_9QR8HGEWHJRGKJHWEKRG
+#define ACCOUNT_INFO_XML_TAG_9QR8HGEWHJRGKJHWEKRG
+
+#include "tinyxml2.h"
+
+class AccountInfo_XmlTag{
+    public:
+        void create_accountingInfo_XMLTAG(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const transferBatch);
+
+    private:
+
+};
+
+#endif //ACCOUNT_INFO_XML_TAG_9QR8HGEWHJRGKJHWEKRG
