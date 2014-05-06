839b8912357d70141d8d9108464584684d4f1990
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-06 17:28:11 +0100

    adding NetworkInfo_XmlTag class

---
+++ NetworkInfo_XmlTag.hpp
@@ -0,0 +1,14 @@
+#ifndef NETWORK_INFO_XML_TAG_P98EHR9G8HE9RGH
+#define NETWORK_INFO_XML_TAG_P98EHR9G8HE9RGH
+
+#include "tinyxml2.h"
+
+class NetworkInfo_XmlTag{
+    public:
+        void create_networkInfo_XMLTAG(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const transferBatch);
+
+    private:
+
+};
+
+#endif //NETWORK_INFO_XML_TAG_P98EHR9G8HE9RGH
