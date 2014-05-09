b5413ff82f220c72d55b6b2e6f9ea5cf10701417
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 15:06:48 +0100

    erasing unused code

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,10 @@
 
 #include "tinyxml2.h"  
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <random>
-#include <chrono>
-
 using namespace std;
 using namespace tinyxml2;
 
 int main(int argc, const char *argv[]){
-
-    vector<string> LOCALISATION_AREA{"LA1", "LA2"};
-
-    for(auto& i:LOCALISATION_AREA) cout<<i<<endl;
-    cout<<endl;
-
-    cout<<"============================"<<endl<<endl;
-
-    const unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-
-    default_random_engine random_generator(seed);
-    std::uniform_int_distribution<unsigned int> distribution_localisation_area(0,LOCALISATION_AREA.size()-1);
-
-    const unsigned int random_loacalisation_area_index = distribution_localisation_area(random_generator);
-
-    cout<<"localisation area: "<<LOCALISATION_AREA[random_loacalisation_area_index]<<endl;
-
-    cout<<"+++++++++++++++++++++++++++++"<<endl<<endl;
-
     XMLDocument doc;
 
     XMLMessageGenerator generator;
@@ -39,8 +14,6 @@ int main(int argc, const char *argv[]){
 
     XMLPrinter printer;
     doc.Print();
-
-    cout<<endl;
 
     return 0;
 }
