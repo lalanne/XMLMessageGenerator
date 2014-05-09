2375e2f218913f156b59ae7554785026eb34b872
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 14:38:16 +0100

    creating a class that generates random imsi numbers from a limited selection

---
+++ include/ImsiGenerator.hpp
@@ -0,0 +1,18 @@
+#ifndef IMSI_GENERATOR_Q9ER8GH9Q8HERG9H8QERGH
+#define IMSI_GENERATOR_Q9ER8GH9Q8HERG9H8QERGH
+
+#include <string>
+#include <vector>
+
+class ImsiGenerator{
+    public:
+        std::string imsi();
+
+    private:
+        std::vector<std::string> imsis{"272993733999123", 
+                                    "272993733999105", 
+                                    "272993799979995", 
+                                    "272993733999905"};
+};
+
+#endif //IMSI_GENERATOR_Q9ER8GH9Q8HERG9H8QERGH
--- src/Imsi_XmlTag.cpp
+++ src/Imsi_XmlTag.cpp
@@ -1,11 +1,16 @@
 
 #include "Imsi_XmlTag.hpp"
 
+#include <string>
+
 using namespace tinyxml2;
+using namespace std;
 
 void Imsi_XmlTag::create_imsi_XMLTAG(XMLDocument& doc, XMLElement* const simChargeableSubscriber){
     XMLElement* imsi = doc.NewElement("imsi");
-    XMLText* imsiText = doc.NewText("");
+    ImsiGenerator imsiGenerator;
+
+    XMLText* imsiText = doc.NewText(imsiGenerator.imsi().c_str());
     imsi->LinkEndChild(imsiText);
     simChargeableSubscriber->LinkEndChild(imsi);
 }
