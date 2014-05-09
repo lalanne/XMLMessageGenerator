32e2bbbbe7b6dccd613e89a323d87506e786c759
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 14:52:58 +0100

    CellIdGenerator

---
+++ include/CellIdGenerator.hpp
@@ -0,0 +1,16 @@
+#ifndef CELLID_GENERATOR_QWGE87QWGEF87GWEF
+#define CELLID_GENERATOR_QWGE87QWGEF87GWEF
+
+#include <string>
+#include <vector>
+
+class CellIdGenerator{
+    public:
+        const char* const cellId();
+
+    private:
+        std::vector<std::string> cellIds{"37107", 
+                                        "4969"};
+};
+
+#endif //CELLID_GENERATOR_QWGE87QWGEF87GWEF
--- src/CellId_XmlTag.cpp
+++ src/CellId_XmlTag.cpp
@@ -1,11 +1,13 @@
 
 #include "CellId_XmlTag.hpp"
+#include "CellIdGenerator.hpp"
 
 using namespace tinyxml2;
 
 void CellId_XmlTag::create(XMLDocument& doc, XMLElement* const networkLocation){
     XMLElement* cellId = doc.NewElement("cellId");
-    XMLText* cellIdText = doc.NewText("");
+    CellIdGenerator cellIdGenerator;
+    XMLText* cellIdText = doc.NewText(cellIdGenerator.cellId());
     cellId->LinkEndChild(cellIdText);
     networkLocation->LinkEndChild(cellId);
 }
