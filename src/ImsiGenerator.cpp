74b560f4ff5b869f79b17f83b995721d00e772aa
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 15:28:45 +0100

    erasing unuse code and couts

--- src/ImsiGenerator.cpp
+++ src/ImsiGenerator.cpp
@@ -3,15 +3,13 @@
 
 #include <chrono>
 #include <random>
-#include <iostream>
 
 using namespace std;
 
 const char* const ImsiGenerator::imsi(){
-    const unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+    const unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
     default_random_engine random_generator(seed);
-    std::uniform_int_distribution<unsigned int> distribution_imsi(0, imsis.size()-1);
+    uniform_int_distribution<unsigned int> distribution_imsi(0, imsis.size()-1);
     const unsigned int random_imsi_index = distribution_imsi(random_generator);
-    cout<<imsis[random_imsi_index].c_str()<<endl;
     return imsis[random_imsi_index].c_str();
 }
--- src/Imsi_XmlTag.cpp
+++ src/Imsi_XmlTag.cpp
@@ -1,10 +1,7 @@
 
 #include "Imsi_XmlTag.hpp"
 
-#include <string>
-
 using namespace tinyxml2;
-using namespace std;
 
 void Imsi_XmlTag::create_imsi_XMLTAG(XMLDocument& doc, XMLElement* const simChargeableSubscriber){
     XMLElement* imsi = doc.NewElement("imsi");
