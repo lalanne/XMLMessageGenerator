b8a5a2b5bc9968fc058257ed0085788dbfa4809f
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 15:52:35 +0100

    removing specific names

--- src/ImsiGenerator.cpp
+++ src/ImsiGenerator.cpp
@@ -9,7 +9,7 @@ using namespace std;
 const char* const ImsiGenerator::imsi(){
     const unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
     default_random_engine random_generator(seed);
-    uniform_int_distribution<unsigned int> distribution_imsi(0, imsis.size()-1);
-    const unsigned int random_imsi_index = distribution_imsi(random_generator);
-    return imsis[random_imsi_index].c_str();
+    uniform_int_distribution<unsigned int> distribution(0, imsis.size()-1);
+    const unsigned int random_index = distribution(random_generator);
+    return imsis[random_index].c_str();
 }
