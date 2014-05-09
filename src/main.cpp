96cbbe290eab685f0768f728a584ce6614cc166f
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 14:56:44 +0100

    erasing unused code

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,8 @@ using namespace tinyxml2;
 
 int main(int argc, const char *argv[]){
 
-    vector<string> CELL_ID{"cell1", "cell2", "cell3", "cell4"};
     vector<string> LOCALISATION_AREA{"LA1", "LA2"};
 
-    cout<<endl;
-    for(auto& i:CELL_ID) cout<<i<<endl;
-    cout<<endl;
     for(auto& i:LOCALISATION_AREA) cout<<i<<endl;
     cout<<endl;
 
@@ -28,13 +24,10 @@ int main(int argc, const char *argv[]){
     const unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
 
     default_random_engine random_generator(seed);
-    std::uniform_int_distribution<unsigned int> distribution_cell_id(0,CELL_ID.size()-1);
     std::uniform_int_distribution<unsigned int> distribution_localisation_area(0,LOCALISATION_AREA.size()-1);
 
-    const unsigned int random_cell_id_index = distribution_cell_id(random_generator);
     const unsigned int random_loacalisation_area_index = distribution_localisation_area(random_generator);
 
-    cout<<"cell_id: "<<CELL_ID[random_cell_id_index]<<endl;
     cout<<"localisation area: "<<LOCALISATION_AREA[random_loacalisation_area_index]<<endl;
 
     cout<<"+++++++++++++++++++++++++++++"<<endl<<endl;
