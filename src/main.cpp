0f1b9b9ff8b313f6a7669ba730cf67eb010f92de
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 15:33:08 +0100

    creating function to create messages

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,18 @@
 
 using namespace tinyxml2;
 
+void generate_message(){
+    XMLDocument doc;
+    XMLMessageGenerator generator;
+    generator.create_message(doc);
+    XMLPrinter printer;
+    doc.Print();
+}
+
 int main(int argc, const char *argv[]){
     const unsigned int number_of_messages = atoi(argv[1]);
 
-    for(unsigned int i=0; i<number_of_messages; ++i){
-        XMLDocument doc;
-        XMLMessageGenerator generator;
-        generator.create_message(doc);
-        XMLPrinter printer;
-        doc.Print();
-    }
-
+    for(unsigned int i=0; i<number_of_messages; ++i) generate_message();
+    
     return 0;
 }
