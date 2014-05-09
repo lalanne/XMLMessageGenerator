75a21d39e2289d9355af5caa02135dd745068430
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-09 16:04:34 +0100

    shortening methods name

--- include/NetworkInfo_XmlTag.hpp
+++ include/NetworkInfo_XmlTag.hpp
@@ -5,10 +5,7 @@
 
 class NetworkInfo_XmlTag{
     public:
-        void create_networkInfo_XMLTAG(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const transferBatch);
-
-    private:
-
+        void create(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const transferBatch);
 };
 
 #endif //NETWORK_INFO_XML_TAG_P98EHR9G8HE9RGH
