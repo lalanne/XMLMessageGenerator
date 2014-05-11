04373b6eb19925f86f6d5c081cf0ea270a7fc7a2
Christian <lalanne@gmail.com>
2014-05-11 19:29:10 +0100

    put the generator as a member field

--- include/LocationArea_XmlTag.hpp
+++ include/LocationArea_XmlTag.hpp
@@ -1,6 +1,8 @@
 #ifndef LOCATION_AREA_XML_TAG_W98HFG9WHEG98HWGE
 #define LOCATION_AREA_XML_TAG_W98HFG9WHEG98HWGE
 
+#include "LocationAreaGenerator.hpp"
+
 #include "tinyxml2.h"
 
 class LocationArea_XmlTag{
@@ -8,6 +10,7 @@ class LocationArea_XmlTag{
         void create(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const networkLocation);
 
     private:
+        LocationAreaGenerator locationAreaGenerator;
 };
 
 #endif //LOCATION_AREA_XML_TAG_W98HFG9WHEG98HWGE
--- src/LocationArea_XmlTag.cpp
+++ src/LocationArea_XmlTag.cpp
@@ -1,12 +1,10 @@
 
 #include "LocationArea_XmlTag.hpp"
-#include "LocationAreaGenerator.hpp"
 
 using namespace tinyxml2;
 
 void LocationArea_XmlTag::create(XMLDocument& doc, XMLElement* const networkLocation){
     XMLElement* locationArea = doc.NewElement("locationArea");
-    LocationAreaGenerator locationAreaGenerator;
     XMLText* locationAreaText = doc.NewText(locationAreaGenerator.locationArea());
     locationArea->LinkEndChild(locationAreaText);
     networkLocation->LinkEndChild(locationArea);
