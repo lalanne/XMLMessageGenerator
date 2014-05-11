5ec7ce781297e664ce0175d7cd11937312418725
Christian <lalanne@gmail.com>
2014-05-11 19:25:13 +0100

    put the generator as a member field

--- include/CellId_XmlTag.hpp
+++ include/CellId_XmlTag.hpp
@@ -1,6 +1,8 @@
 #ifndef CELL_ID_XML_TAG_9W8HRG9WHGOWHGLIHWG
 #define CELL_ID_XML_TAG_9W8HRG9WHGOWHGLIHWG
 
+#include "CellIdGenerator.hpp"
+
 #include "tinyxml2.h"
 
 class CellId_XmlTag{
@@ -8,6 +10,7 @@ class CellId_XmlTag{
         void create(tinyxml2::XMLDocument& doc, tinyxml2::XMLElement* const networkLocation);
 
     private:
+        CellIdGenerator cellIdGenerator;
 };
 
 #endif //CELL_ID_XML_TAG_9W8HRG9WHGOWHGLIHWG
--- src/CellId_XmlTag.cpp
+++ src/CellId_XmlTag.cpp
@@ -1,12 +1,10 @@
 
 #include "CellId_XmlTag.hpp"
-#include "CellIdGenerator.hpp"
 
 using namespace tinyxml2;
 
 void CellId_XmlTag::create(XMLDocument& doc, XMLElement* const networkLocation){
     XMLElement* cellId = doc.NewElement("cellId");
-    CellIdGenerator cellIdGenerator;
     XMLText* cellIdText = doc.NewText(cellIdGenerator.cellId());
     cellId->LinkEndChild(cellIdText);
     networkLocation->LinkEndChild(cellId);
