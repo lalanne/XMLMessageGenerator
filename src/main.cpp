f5861184bf5fdbb9b264e1d53342211a2124f2d6
Christian Lalanne <christian.lalanne@ichec.ie>
2014-05-12 10:17:51 +0100

    introducing error handling for incorrect number of arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 
 #include "tinyxml2.h"  
 
+#include <iostream>
+
 using namespace tinyxml2;
+using namespace std;
+
 
 void generate_message(){
     XMLDocument doc;
@@ -13,10 +17,18 @@ void generate_message(){
     doc.Print();
 }
 
-int main(int argc, const char *argv[]){
-    const unsigned int number_of_messages = atoi(argv[1]);
+void print_error_message(){
+    cerr<<"ERROR: invalid number of arguments."<<endl;
+    cerr<<"usage: ./generator <number of messages>"<<endl;
+}
 
+void generate_x_number_of_messages(const unsigned int number_of_messages){
     for(unsigned int i=0; i<number_of_messages; ++i) generate_message();
+}
+
+int main(int argc, const char *argv[]){
+    if(argc != 2) print_error_message();
+    else generate_x_number_of_messages(atoi(argv[1]));
     
     return 0;
 }
